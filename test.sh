fe89d47c8e373f68a877952981f5bd39390f9016
A.Lepe <lepe@support.ne.jp>
2023-04-07 10:10:11 +0900

    added random bot selection in test.sh

--- test.sh
+++ test.sh
@@ -17,14 +17,19 @@ else
 fi
 
 if [[ $1 == "me" ]]; then
-vs="--human 1"
-speed=25
-showall="--show_all"
+	vs="--human 1"
+	speed=25
+	showall="--show_all"
 else
-speed=10
-#vs="Feint"
-vs="MadMixT"
-#vs="Hao Pan"
+	speed=10
+	vs=$(shuf bots.lst | head -n 1)
+	if [[ $2 != "" ]]; then
+		vs="$2"
+	elif [[ $1 != "" ]]; then
+		if grep "$1" bots.lst; then
+			vs="$1"
+		fi
+	fi
 fi
 
 if [[ $1 == "show" ]]; then
@@ -32,16 +37,17 @@ showall="--show_all"
 fi
 
 if [[ $1 == "hidden" || $DISPLAY == "" ]]; then
-arg="--headless"
-#mapdir="/$home_dir/maps/BroodWar"
-#map="(8)Big Game Hunters.scm"
-mapdir="/$home_dir/maps/2019Season2"
-map="(2)Tres Pass.scx"
+	arg="--headless"
+	#mapdir="/$home_dir/maps/BroodWar"
+	#map="(8)Big Game Hunters.scm"
+	mapdir="/$home_dir/maps/2019Season2"
+	map="(2)Overwatch(n).scx"
 else
-arg="--capture_movement"
-mapdir="/$home_dir/maps/"
-map="manual-choose"
-speed=${speed:-15}
+	arg="--capture_movement"
+	mapdir="/$home_dir/maps/"
+	map="manual-choose"
+	speed=${speed:-15}
 fi
+echo "Playing against: $vs"
 scbw.play --bots "Zorg" "$vs" $arg --game_speed $speed --map_dir "$mapdir" --map "$map" $showall
 pkill log.sh
